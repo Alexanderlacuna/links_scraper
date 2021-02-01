683d96d2e5cc8b91705a131e1f6fdfb72876f718
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-01 16:31:12 +0300

    add ability to find broken links recursively

--- genelinks.py
+++ genelinks.py
@@ -2,7 +2,7 @@ import re
 import requests
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-
+from urllib.parse import urljoin
 
 def test_link(link):
     print(f"link testing {link}")
@@ -17,14 +17,45 @@ def test_link(link):
     status_code = results.status_code if results is not None else "404"
 
     print(f'the link {link} ---> {status_code}')
+def call_links_recursively(page_url):
+
+    '''modified function to call internal links recursively '''
+    internal_links = set()
+    html_page = uReq(page_url)
+    parsed_page = soup(html_page,"html.parser")
+
+    for link in parsed_page.findAll("a"):
+        link_url = links.attrs.get(href)
+
+        if re.match(r"^/",href):
+            # means it's an internal link
+
+            full_path = urljoin('https://www.genenetwork.org/',href)
+            internal_links.add(full_path)
+
+        elif  re.match(r'^http://',href):
+            # external link
+            full_path= href
+
+
+        test_link(full_path)
+
+
+    for link in internal_links:
+        call_links_recursively(link)
+
+
+
 
 def scraper_for_webpage(page_url):
+
+    '''function for only links that starts with http '''
     html_page = uReq(page_url)
     parsed_page = soup(html_page, "html.parser")
 
     for link in parsed_page.findAll("a", attrs={"href": re.compile("^http://")}):
+        print("calling internal_links")
         test_link(link.get("href"))
 
 
-
-scraper_for_webpage("https://www.genenetwork.org/")
+call_links_recursively("https://www.genenetwork.org/")
