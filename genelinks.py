b75f2ccb33f77dfc7c024d255a882b887ab8d7ad
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-02 00:06:21 +0300

    rename functions

--- genelinks.py
+++ genelinks.py
@@ -1,8 +1,11 @@
 import re
 import requests
+import urllib3
+
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from urllib.parse import urljoin
+
 
 def test_link(link):
     print(f"link testing {link}")
@@ -17,42 +20,40 @@ def test_link(link):
     status_code = results.status_code if results is not None else "404"
 
     print(f'the link {link} ---> {status_code}')
-def call_links_recursively(page_url,visited=set()):
 
+
+def fetch_page_links(page_url):
     '''modified function to call internal links recursively '''
+
+    visited =set()
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
     internal_links = set()
     html_page = uReq(page_url)
-    parsed_page = soup(html_page,"html.parser")
+    parsed_page = soup(html_page, "html.parser")
 
     for link in parsed_page.findAll("a"):
         link_url = link.attrs.get("href")
 
-        if re.match(r"^/",link_url):
+        if re.match(r"^/", link_url):
             # means it's an internal link
 
-            full_path = urljoin('https://www.genenetwork.org/',link_url)
+            full_path = urljoin('https://www.genenetwork.org/', link_url)
             if link not in visited:
 
                 internal_links.add(full_path)
 
-        elif  re.match(r'^http://',link_url):
+        elif re.match(r'^http://', link_url):
             # external link
-            full_path= link_url
+            full_path = link_url
 
         if link not in visited:
             visited.add(link)
             test_link(full_path)
 
 
-    # for link in internal_links:
-
-    #     call_links_recursively(link,visited=visited)
-
-
-
-
 def scraper_for_webpage(page_url):
-
     '''function for only links that starts with http '''
     html_page = uReq(page_url)
     parsed_page = soup(html_page, "html.parser")
@@ -61,4 +62,4 @@ def scraper_for_webpage(page_url):
         test_link(link.get("href"))
 
 
-call_links_recursively("https://www.genenetwork.org/")
+fetch_page_links("https://www.genenetwork.org/")
