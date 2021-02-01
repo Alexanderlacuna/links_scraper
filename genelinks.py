44210d58b5b403808a8d3bcf0611301721ea13d6
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-01 15:21:55 +0300

    pep8 formatting

--- genelinks.py
+++ genelinks.py
@@ -1,42 +1,37 @@
 import re
 import requests
 from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup  as soup
-
-
-
+from bs4 import BeautifulSoup as soup
 
 
 def test_link(link):
-	print(f"link testing {link}")
-	results = None
-	try:
+    print(f"link testing {link}")
+    results = None
+    try:
 
-		results= requests.get(link, verify=False, timeout=10)
+        results = requests.get(link, verify=False, timeout=10)
 
-	except Exception as e:
-		print(f"failed for request {link}")
+    except Exception as e:
+        print(f"failed for request {link}")
 
-	# print(results.status_code)
+    status_code = results.status_code if results is not None else "404"
 
-	status_code = results.status_code if results is not None else  "404"
- 
-	print(f'the link {link}--->{status_code}')
-html_page =uReq("https://www.genenetwork.org/")
-# html_page.close()
-soup =  soup(html_page,"html.parser")
-for link in soup.findAll("a",attrs={'href': re.compile("^http://")}):
-	# print(link.get('href'))
-	test_link(link.get("href"))
+    print(f'the link {link}--->{status_code}')
 
 
+html_page = uReq("https://www.genenetwork.org/")
+soup = soup(html_page, "html.parser")
+for link in soup.findAll("a", attrs={'href': re.compile("^http://")}):
+    test_link(link.get("href"))
 
 
 def scraper_for_webpage(page_url):
-	html_page = uReq("https://www.genenetwork.org/")
-	parsed_page = soup(html_page,"html.parser")
+    html_page = uReq(page_url)
+    parsed_page = soup(html_page, "html.parser")
 
-	for link in parsed_page.findAll("a",attrs={"href":re.compile("^http://")}):
-		print(link)
-		test_link(link.get("href"))
+    for link in parsed_page.findAll("a", attrs={"href": re.compile("^http://")}):
+        print(link)
+        test_link(link.get("href"))
 
+
+scraper_for_webpage(page_url="https://www.genenetwork.org/")
